1686dad945f97fcd7fed256a84d67f95d3559834
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2021-03-26 13:58:58 +0530

    Updated roles

--- backend/src/config/roles.js
+++ backend/src/config/roles.js
@@ -1,8 +1,8 @@
 const roles = ['user', 'admin'];
 
 const roleRights = new Map();
-roleRights.set(roles[0], ['getUsers', 'sendMessages', 'getMessages']);
-roleRights.set(roles[1], ['getUsers', 'manageUsers', 'sendMessages', 'getMessages', 'manageMessages']);
+roleRights.set(roles[0], ['getUsers', 'sendMessages', 'getMessages', 'manageRooms', 'getRooms']);
+roleRights.set(roles[1], ['getUsers', 'manageUsers', 'sendMessages', 'getMessages', 'manageMessages', 'getRooms', 'manageRooms', 'createRooms']);
 
 module.exports = {
   roles,
