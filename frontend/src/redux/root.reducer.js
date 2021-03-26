5177f4c0c525c447b111ceaa53dce2bea84c5751
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2021-03-27 03:08:37 +0530

    Bug fix

--- frontend/src/redux/root.reducer.js
+++ frontend/src/redux/root.reducer.js
@@ -3,12 +3,14 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import savedStateReducer from './saved-state/saved-state.reducer';
 
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
 
 const rootReducer = combineReducers({
     user: userReducer,
+    savedState: savedStateReducer,
 });
 
 const persistConfig = {
