cd6d793760afec6c60a25c119a3b92c13b90f041
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2021-05-25 00:54:15 +0530

    remove unwanted folders

--- frontend-deploy-netlify/src/redux/user/user.selector.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-const { createSelector } = require('reselect');
-
-const selectUser = state => state.user;
-
-const selectCurrentUser = createSelector([selectUser], user => user.currentUser);
-
-export { selectCurrentUser };
--- frontend-deploy-netlify/src/redux/user/user.types.js
+++
@@ -1,5 +0,0 @@
-/** @format */
-
-export const UserActionTypes = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER',
-};
